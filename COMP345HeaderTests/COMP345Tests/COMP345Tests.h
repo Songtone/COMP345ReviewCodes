1cba5e458e692c4ffe23f953c588bddd905af232
Sebastien Ong Tone <s.ongtone@hotmail.com>
2017-10-18 19:47:21 -0400

    cleaning stuff

---
+++ COMP345HeaderTests/COMP345Tests/COMP345Tests.h
@@ -0,0 +1,16 @@
+#ifndef COMP345TESTS_H
+#define COMP345TETS_H
+
+
+#include <iostream>;
+//using namespace std;
+using std::cout;
+using std::cin;
+
+int x;
+int y;
+int z;
+
+int math(int first, int second);
+
+#endif
