85f2492188770762c767174c529cc41704e951b9
Sebastien Ong Tone <s.ongtone@hotmail.com>
2017-12-15 00:25:06 -0500

    created an isInArray function

--- COMP345Question3/COMP345Question3/COMP345Question3.cpp
+++ COMP345Question3/COMP345Question3/COMP345Question3.cpp
@@ -10,15 +10,19 @@ using namespace std;
 class MyArray {
 public:
 	MyArray();//default constructor
-	MyArray(int arrayNew[],int arrayOld[]);//constructor that takes arrays as arguments
+	MyArray(int arrayNew[], int arrayOld[]);//constructor that takes arrays as arguments
 	MyArray(const MyArray& m);//copy constructor
+
+	~MyArray();//destructor
+
 	bool isInArray(int number);
 	void operator = (const MyArray &m);
+	void getArray();
+	void getSize();
 
 private:
 	int* theArray;
 	int size;
-	string s;
 
 };
 
@@ -27,28 +31,56 @@ int main()
 {
 	MyArray();
 	system("pause");
-    return 0;
+	return 0;
 }
 
 MyArray::MyArray() {
 	size = 0;
 	theArray = new int[size];
-	s = "hey";
-	cout << s;
+
 }
 MyArray::MyArray(int array1[], int arrayOld[]) {
 
+	for (int i = 0; i < sizeof(array1) / sizeof(array1[0]); i++) {
+		if (isInArray(array1[i])) {
+
+		}
+		else
+		{
+			arrayOld[size + 1] = array1[i];
+		}
+	}
 
 }
 MyArray::MyArray(const MyArray& m) {
 	theArray = m.theArray;
 	size = m.size;
 }
+MyArray::~MyArray() {
+	delete theArray;
+}
 bool MyArray::isInArray(int number) {
+	for (int i = 0; i < size; i++) {
+		if (number == theArray[i]) {
+			return true;
+			break;
+		}
+		else {
+			return false;
+		}
+
+	}
 
 }
 void MyArray::operator = (const MyArray &m) {
 	theArray = m.theArray;
 	size = m.size;
+}
+void getArray() {
+
+
+}
+void getSize() {
+
 }
 
