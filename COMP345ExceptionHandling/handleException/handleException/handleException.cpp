133cba9740ec8435fd94745f826377d9fe20803a
Sebastien Ong Tone <s.ongtone@hotmail.com>
2017-12-19 01:09:30 -0500

    Added some more reviews

---
+++ COMP345ExceptionHandling/handleException/handleException/handleException.cpp
@@ -0,0 +1,30 @@
+// handleException.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+using namespace std;
+
+double division(int a, int b) {
+	if (b == 0) {
+		throw "Division by zero condition!";
+	}
+	return (a / b);
+}
+
+int main() {
+	int x = 50;
+	int y = 0;
+	double z = 0;
+
+	try {
+		z = division(x, y);
+		cout << z << endl;
+	}
+	catch (const char* msg) {
+		cerr << msg << endl;
+	}
+	system("pause");
+	return 0;
+}
+
